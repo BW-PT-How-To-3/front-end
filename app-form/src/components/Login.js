4171c6564deb646651c9c849bd74176d1edca1a8
matieuruth <matieu.ruth@gmail.com>
2020-09-16 20:20:42 -0400

    installed axios

--- app-form/src/components/Login.js
+++ app-form/src/components/Login.js
@@ -1,32 +1,58 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function Login() {
 
+    const [userInput, setUserInput] = useState({
+        name: '',
+        password: ''
+    });
+
+    const submitHandler = e => {
+        e.preventDefault();
+
+        setUserInput({
+            name: '',
+            password: ''
+        })
+    }
+
+    const changeHandler = e => {
+        e.persist();
+        // setUserInput({...userInput, [e.target.name]: value});
+    }
+
     return(
         <div>
-            <form onSubmit={null}>
-                <h1>Login</h1>
-                <p>Login here using your username and password</p>
+            <form onSubmit={submitHandler}>
+                <h1>Log In</h1>
+                <p>Log in here using your username and password</p>
 
-                <label htmlFor="username">
-                    <input
-                        type="text"
-                        name="username"
-                        value=""
-                        onChange=""
-                        placeholder="@username"
-                    />
-                </label>
+                <div className="form-input">
+                    <label htmlFor="username">
+                        {/* Username */}
+                        <input
+                            type="text"
+                            name="username"
+                            value={userInput.name}
+                            onChange={changeHandler}
+                            placeholder="username"
+                        />
+                    </label>
 
-                <label htmlFor="password">
-                    <input
-                        type="password"
-                        name="password"
-                        value=""
-                        onChange=""
-                    />
-                </label>
+                    <label htmlFor="password">
+                        {/* Password */}
+                        <input
+                            type="password"
+                            name="password"
+                            value={userInput.password}
+                            onChange={changeHandler}
+                            placeholder="password"
+                        />
+                    </label>
+                </div>
+                <button>Log In</button>
             </form>
+            <button>Sign Up</button>
         </div>
     )
 }
