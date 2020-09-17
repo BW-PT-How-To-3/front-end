96ea5b4a3339ab80cb5fb6f064aaf689993ac517
matieuruth <matieu.ruth@gmail.com>
2020-09-16 20:01:32 -0400

    username/password input created

--- app-form/src/components/Login.js
+++ app-form/src/components/Login.js
@@ -1,8 +1,33 @@
 import React from 'react';
 
 function Login() {
+
     return(
-        <h1>Login</h1>
+        <div>
+            <form onSubmit={null}>
+                <h1>Login</h1>
+                <p>Login here using your username and password</p>
+
+                <label htmlFor="username">
+                    <input
+                        type="text"
+                        name="username"
+                        value=""
+                        onChange=""
+                        placeholder="@username"
+                    />
+                </label>
+
+                <label htmlFor="password">
+                    <input
+                        type="password"
+                        name="password"
+                        value=""
+                        onChange=""
+                    />
+                </label>
+            </form>
+        </div>
     )
 }
 
