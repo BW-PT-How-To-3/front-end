f54ef0ffd6928979ab921025954d1b60b53988f4
matieuruth <matieu.ruth@gmail.com>
2020-09-20 21:26:16 -0400

    update login

--- app-form/src/component/Login.js
+++ app-form/src/component/Login.js
@@ -98,47 +98,53 @@ function Login() {
     };
 
     return(
-        <div className="form-input">
-            <h1 className="app-bar">Welcome back!</h1>
-            <p>Log in using your username and password</p>
-            <form onSubmit={submitHandler}>
+        <div className="signup-div">
+            <div>  
+                <div className="form-info">  
+                    <h1 className="app-bar">Welcome back!</h1>
+                    <p className="member-login-login">Log in using your username and password</p>
+                        <div className="form-input">  
+                            <form onSubmit={submitHandler}>
+                            
+                                <div>
+                                        <label htmlFor="username"></label><br/>
+                                        <TextField
+                                            id="standard-basic" 
+                                            label="Username"
+                                            type="text"
+                                            name="username"
+                                            fullWidth
+                                            value={userInput.username}
+                                            onChange={changeHandler}
+                                            helperText={errorState.username.length > 0 ? (<p className="error">{errorState.username}</p>) : null}
+                                        /><br/>
 
-                <div>
-                    <MuiThemeProvider>
-                        <TextField
-                            type="text"
-                            id="username"
-                            name="username"
-                            value={userInput.username}
-                            onChange={changeHandler}
-                            placeholder="Username"
-                        /><br/>
-                        <label htmlFor="username">
-                            {/*Username*/}
-                            {errorState.username.length > 0 ? (<p className="error">{errorState.username}</p>) : null}
-                        </label><br/>
-                    </MuiThemeProvider>
-
-                    <MuiThemeProvider>
-                        
-                        <TextField
-                            type={passwordInputType}
-                            id="password"
-                            name="password"
-                            value={userInput.password}
-                            onChange={changeHandler}
-                            placeholder="Password"
-                        /><br/>
-                        <span className="password-icon">{ToggleIcon}</span>
-                        <label htmlFor="password"> {/*Password*/}
-                            {errorState.password.length > 0 ? (<p className="error">{errorState.password}</p>) : null}
-                        </label><br/>                        
-                    </MuiThemeProvider>
-                </div>
-                <button style={styles.button} disabled={buttonDisabled}>Log In</button>
-                <pre>{JSON.stringify(data, null, 2)}</pre>
-            </form>
-            <p>Don't have an account? <span>Sign Up</span></p>
+                                        <label htmlFor="password"></label><br/> 
+                                        
+                                        <TextField
+                                            type={passwordInputType}
+                                            id="standard-basic" 
+                                            label="Password"
+                                            name="password"
+                                            fullWidth
+                                            value={userInput.password}
+                                            onChange={changeHandler}
+                                            helperText={errorState.password.length > 0 ? (<p className="error">{errorState.password}</p>) : null}
+                                        />
+                                        <div><span className="password-icon-login">{ToggleIcon}</span></div>
+                                </div>
+                                <button disabled={buttonDisabled} className="login-btn">Log In</button>
+                                <p>Don't have an account? <Link to="/sign-up" style={styles.links}>Sign Up</Link></p>
+                                {/* <pre>{JSON.stringify(data, null, 2)}</pre> */}
+                            </form>
+                        </div>  
+                    </div>  
+                </div>    
+            <div className="form-graphics">
+                    <p>Find your next project.<br/>Engange your followers.<br/>
+                        Do it all with <span>HowTo</span>.
+                    </p>
+            </div>
         </div>
     )
 }
