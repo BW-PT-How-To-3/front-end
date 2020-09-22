156040a034338669c51bf0d0eed573cc4109b8f7
Shirley Lee <synchrotronflux@yahoo.com>
2020-09-21 22:01:42 -0400

    combined

--- app-form/src/component/DeleteUser.js
+++ app-form/src/component/DeleteUser.js
@@ -1,37 +1,31 @@
 import React, { useState, useEffect } from "react";
 // import axios from "axios";
-import { axiosWithAuth } from '../utils/axiosWithAuth';
+import { axiosWithAuth } from "../utils/axiosWithAuth";
 
 const user = {
-  username:'',
-  password:''
- }
+  username: "",
+  password: "",
+};
 
 function DeleteUser(props) {
   const [userData, setUserData] = useState(user);
-  console.log(userData)
-
+  console.log(userData);
 
   const handleClick = (id) => {
     axiosWithAuth()
-    .delete(`/api/users/delete/${id}`).then(res => {
-      userData.filter(users => {
-        users.id !== id;
+      .delete(`/api/users/delete/${id}`)
+      .then((res) => {
+        userData.filter((users) => {
+          users.id !== id;
+        });
       })
-      })
-      .catch(err => {
-        console.log(err)
-      })
+      .catch((err) => {
+        console.log(err);
+      });
   };
 
-  useEffect(() => {
-    
-  }, []);
+  useEffect(() => {}, []);
 
-  return (
-    <div>
-       
-    </div>
-  );
+  return <div></div>;
 }
 export default DeleteUser;
